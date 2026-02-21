bdb7051df3fb1bbe813e1e2746ed87e1120d877f
nanangriyadi <3053485+nanangriyadi@users.noreply.github.com>
2026-02-22 00:56:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
 const CONFIG = {
   // Paste URL deployment Google Apps Script Anda di sini
   // Contoh: 'https://script.google.com/macros/s/AKfycbxXXXXX.../exec'
-  API_URL: 'GANTI_DENGAN_URL_GAS_ANDA',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxR-pS9hWN4eTezbiV9l7f71dxFUlaGqebAZx1mCwKLpKkRcFvrIAzRilNlF4iT7zlq/exec',
 
   // Nama aplikasi
-  APP_NAME: 'https://script.google.com/macros/s/AKfycbxR-pS9hWN4eTezbiV9l7f71dxFUlaGqebAZx1mCwKLpKkRcFvrIAzRilNlF4iT7zlq/exec',
+  APP_NAME: 'Tracer Alumni',
   SCHOOL_NAME: 'SLB Negeri Badegan Ponorogo', // ← sesuaikan
 };
