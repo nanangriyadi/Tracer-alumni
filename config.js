b742bd91f5e4a5d7119cd691e6f1cfd1c88f7caf
nanangriyadi <3053485+nanangriyadi@users.noreply.github.com>
2026-02-22 16:36:01 +0700

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 const CONFIG = {
   // Paste URL deployment Google Apps Script Anda di sini
   // Contoh: 'https://script.google.com/macros/s/AKfycbxXXXXX.../exec'
-  API_URL: 'https://script.google.com/macros/s/AKfycbwKrscuLweFc4gIMVUBX4PWMqat-En2jtoqX-w1AaWL_mRi2HjZTvOyV_qWXZlS5V4a/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycby7FIn2jWJEcWT7aO1iex5hGywE_o6pTJ94cB3FqmExVgcAC4bGQKco6MukU7lUl3A/exec',
 
   // Nama aplikasi
    // Nama aplikasi
